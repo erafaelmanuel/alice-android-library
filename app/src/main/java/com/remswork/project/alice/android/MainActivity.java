0140f04c240fb26598c19d62e3dd941083be430c
Rafael Manuel <erafaelmanuel@gmail.com>
2017-09-02 02:32:57 +0800

    Implement the grading factor services.

--- app/src/main/java/com/remswork/project/alice/android/MainActivity.java
+++ app/src/main/java/com/remswork/project/alice/android/MainActivity.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
+import com.remswork.project.alice.model.Activity;
+import com.remswork.project.alice.service.ActivityService;
+import com.remswork.project.alice.service.impl.ActivityServiceImpl;
 import com.remswork.project.alice.service.impl.SubjectServiceImpl;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,8 +17,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         try {
-            int size = new SubjectServiceImpl().getSubjectListByTeacherId(124345).size();
-            Log.i("myTAG", size + "");
+            ActivityService activityService = new ActivityServiceImpl();
+            Activity activity = activityService.addActivity(new Activity("TItle", "date", 12, 10), 87, 10101);
+            Log.i("Title", activity.getTitle());
+            //activity = activityService.updateActivityById(activity.getId(), new Activity("TItle", "date", 12, 10), 87, 10101);
+            Log.i("Title", activity.getTitle());
+            activity = activityService.getActivityById(activity.getId());
+            Log.i("Title", activity.getTitle());
+            int size = activityService.getActivityList().size();
+            Log.i("Size", size + "");
+            size = activityService.getActivityListByStudentAndSubjectId(87, 10101).size();
+            Log.i("Size", size + "");
+            size = activityService.getActivityListByStudentAndSubjectId(87, 10212).size();
+            Log.i("Size", size + "");
+            activityService.deleteActivityById(activity.getId());
+            size = activityService.getActivityList().size();
+            Log.i("Size", size + "");
         } catch (Exception e) {
             e.printStackTrace();
         }
